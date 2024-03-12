db558120f2244a319e9caeefd1078d9dd8888adf
Matheus Wisch <wisch.pg@outlook.com>
2024-03-12 20:15:05 -0300

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+   MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -19,48 +26,57 @@ class MyApp extends StatelessWidget {
           title: Text('Tarefas'),
           backgroundColor: Colors.blue,
         ),
-        body: ListView(
-          children: const [
-            Task(
-              nome: 'Academia',
-              foto:
-                  'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-              dificuldade: 3,
-            ),
-            Task(
-              nome: 'Faculdade',
-              foto:
-                  'https://cdn.pixabay.com/photo/2018/01/17/07/06/laptop-3087585_1280.jpg',
-              dificuldade: 5,
-            ),
-            Task(
-              nome: 'Comer',
-              foto:
-                  'https://cdn.pixabay.com/photo/2017/01/26/02/06/platter-2009590_640.jpg',
-              dificuldade: 1,
-            ),
-            Task(
-              nome: 'Dormir',
-              foto:
-                  'https://cdn.pixabay.com/photo/2012/03/01/01/42/baby-20339_640.jpg',
-              dificuldade: 2,
-            ),
-            Task(
-              nome: 'Jogar',
-              foto:
-                  'https://cdn.pixabay.com/photo/2017/01/13/09/23/magic-cube-1976725_1280.jpg',
-              dificuldade: 1,
-            ),
-            Task(
-              nome: 'Alguma coisa',
-              foto:
-                  'https://cdn.pixabay.com/photo/2018/05/12/15/46/rust-3393715_1280.jpg',
-              dificuldade: 4,
-            ),
-          ],
+        body: AnimatedOpacity(
+          duration: Duration(milliseconds: 500),
+          opacity: opacidade ? 1 : 0,
+          child: ListView(
+            children: const [
+              Task(
+                nome: 'Academia',
+                foto:
+                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                dificuldade: 3,
+              ),
+              Task(
+                nome: 'Faculdade',
+                foto:
+                    'https://cdn.pixabay.com/photo/2018/01/17/07/06/laptop-3087585_1280.jpg',
+                dificuldade: 5,
+              ),
+              Task(
+                nome: 'Comer',
+                foto:
+                    'https://cdn.pixabay.com/photo/2017/01/26/02/06/platter-2009590_640.jpg',
+                dificuldade: 1,
+              ),
+              Task(
+                nome: 'Dormir',
+                foto:
+                    'https://cdn.pixabay.com/photo/2012/03/01/01/42/baby-20339_640.jpg',
+                dificuldade: 2,
+              ),
+              Task(
+                nome: 'Jogar',
+                foto:
+                    'https://cdn.pixabay.com/photo/2017/01/13/09/23/magic-cube-1976725_1280.jpg',
+                dificuldade: 1,
+              ),
+              Task(
+                nome: 'Alguma coisa',
+                foto:
+                    'https://cdn.pixabay.com/photo/2018/05/12/15/46/rust-3393715_1280.jpg',
+                dificuldade: 4,
+              ),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+            });
+          },
+          child: Icon(Icons.remove_red_eye),
         ),
       ),
     );
@@ -93,24 +109,34 @@ class _TaskState extends State<Task> {
         child: Stack(
           children: [
             Container(
-              color: Colors.blue,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6), color: Colors.blue),
               height: 140,
             ),
             Column(
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.white,
+                  ),
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.black38,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          color: Colors.black38,
+                        ),
                         width: 72,
                         height: 100,
-                        child: Image.network(
-                          widget.foto,
-                          fit: BoxFit.cover,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.foto,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                       Column(
