7e8e354aaae71822955324279deaa90cb03a2098
Matheus Wisch <wisch.pg@outlook.com>
2024-03-13 11:10:13 -0300

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'screens/initial_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-   MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool opacidade = true;
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,216 +16,12 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         useMaterial3: false,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Tarefas'),
-          backgroundColor: Colors.blue,
-        ),
-        body: AnimatedOpacity(
-          duration: Duration(milliseconds: 500),
-          opacity: opacidade ? 1 : 0,
-          child: ListView(
-            children: const [
-              Task(
-                nome: 'Academia',
-                foto:
-                    'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                dificuldade: 3,
-              ),
-              Task(
-                nome: 'Faculdade',
-                foto:
-                    'https://cdn.pixabay.com/photo/2018/01/17/07/06/laptop-3087585_1280.jpg',
-                dificuldade: 5,
-              ),
-              Task(
-                nome: 'Comer',
-                foto:
-                    'https://cdn.pixabay.com/photo/2017/01/26/02/06/platter-2009590_640.jpg',
-                dificuldade: 1,
-              ),
-              Task(
-                nome: 'Dormir',
-                foto:
-                    'https://cdn.pixabay.com/photo/2012/03/01/01/42/baby-20339_640.jpg',
-                dificuldade: 2,
-              ),
-              Task(
-                nome: 'Jogar',
-                foto:
-                    'https://cdn.pixabay.com/photo/2017/01/13/09/23/magic-cube-1976725_1280.jpg',
-                dificuldade: 1,
-              ),
-              Task(
-                nome: 'Alguma coisa',
-                foto:
-                    'https://cdn.pixabay.com/photo/2018/05/12/15/46/rust-3393715_1280.jpg',
-                dificuldade: 4,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              opacidade = !opacidade;
-            });
-          },
-          child: Icon(Icons.remove_red_eye),
-        ),
-      ),
+      home: InitialScreen(),
     );
   }
 }
 
-class Task extends StatefulWidget {
-  final String nome;
-  final String foto;
-  final int dificuldade;
 
-  const Task(
-      {super.key,
-      required this.nome,
-      required this.foto,
-      required this.dificuldade});
 
-  @override
-  State<Task> createState() => _TaskState();
-}
 
-class _TaskState extends State<Task> {
-  int nivel = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(6), color: Colors.blue),
-              height: 140,
-            ),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(6),
-                    color: Colors.white,
-                  ),
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(6),
-                          color: Colors.black38,
-                        ),
-                        width: 72,
-                        height: 100,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(4),
-                          child: Image.network(
-                            widget.foto,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                              width: 200,
-                              child: Text(
-                                widget.nome,
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                              )),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 1)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 2)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 3)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 4)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: (widget.dificuldade >= 5)
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      ElevatedButton(
-                          onPressed: () {
-                            setState(() {
-                              nivel++;
-                            });
-                            print(nivel);
-                          },
-                          child: Icon(Icons.arrow_drop_up))
-                    ],
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 200,
-                        child: LinearProgressIndicator(
-                          color: Colors.white,
-                          value: (widget.dificuldade > 0)
-                              ? (nivel / widget.dificuldade) / 10
-                              : 1,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text('Nível: $nivel',
-                          style: TextStyle(color: Colors.white, fontSize: 20)),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
